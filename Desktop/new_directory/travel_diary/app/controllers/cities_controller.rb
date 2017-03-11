c582698597abbe189d5549996c8836b04327f09d
“odelaney” <odelaney@tcd.ie>
2017-03-11 13:59:05 +0000

    image uploads

--- Desktop/new_directory/travel_diary/app/controllers/cities_controller.rb
+++ Desktop/new_directory/travel_diary/app/controllers/cities_controller.rb
@@ -69,7 +69,7 @@ class CitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def city_params
-      params.fetch(:city, {}) .permit(:name)
+      params.fetch(:city, {}) .permit(:name, :picture)
 
     end
 end
